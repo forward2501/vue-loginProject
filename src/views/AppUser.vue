cd75ba5066ef2493c37df258ac20ab45f4c4ff34
ali <2501.aliforward@gmail.com>
2022-03-04 18:22:23 +0800

    update

--- src/views/AppUser.vue
+++ src/views/AppUser.vue
@@ -1,6 +1,15 @@
 <template>
-<div :class="['userDiv']"><div>当前登录名：{{getCurrentLoginUser}}</div>
-<button @click='add()'>添加新闻</button>
+<div :class="['background']">
+   <div :class="['userinfo-div']">当前用户：<img src="../assets/user.png" :style="{width:'50'+'px',height:'50'+'px'}">{{getCurrentLoginUser}}
+   </div>
+   <div :class="['functionDiv']">
+      <ul>
+        <li @click='add()'><a>发表新闻<span>>></span></a></li>
+        <li><a>修改密码<span>>></span></a></li>
+         <li><a>修改头像<span>>></span></a></li>
+        <li><a>退出登录<span>>></span></a></li>
+      </ul>
+   </div>
 </div>
 </template>
 <script>
@@ -26,16 +35,54 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.userDiv{
-  margin: 20px auto;
-  width: 500px;
-  height: 100px;
+.background{
+  left:0;
+  top:0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index:-1;
+ background-color: rgb(238, 235, 235);
+  background-size: 100%;
 }
-button{
- margin:20px;
- width: 100px;
- height: 30px;
- background-color: rgba(39, 39, 214, 0.8);
- color:#fff
+.userinfo-div{
+ position: fixed;
+ right:10px;
+ top:10px;
+}
+.functionDiv{
+  margin: 100px auto;
+  width: 1000px;
+  height: 800px;
+  font-size:18px;
+  position: relative;
+}
+ul{
+  //text-align:center;
+  width: 600px;
+  height: 600px;
+  position: absolute;
+  top:50%;
+  margin-top: -300px;
+  left:50%;
+  margin-left:-300px;
+}
+ul li{
+  list-style: none;
+  width: 400px;
+  height: 60px;
+  background-color: #42b983;;//#42b983;
+  border-bottom: 2px solid rgb(184, 181, 179);;
+  font-size:18px;
+  padding:20px;
+  line-height: 60px;
+  color:#fff;
+  span{
+  display:inline-block;
+  width: 50px;
+  height: 60px;
+  float: right;
+  color:#fff;
+  }
 }
 </style>
